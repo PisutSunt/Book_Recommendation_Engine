4f2d2b9712dddb286e68a98397dee447752f00ea
Pisut-1037 <pisut.peter@mail.kmutt.ac.th>
2020-04-05 04:42:15 +0700

    text file reader class

---
+++ TextFileReader.java
@@ -0,0 +1,66 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class TextFileReader
+{
+    private BufferedReader reader = null;
+
+    public boolean openFile(String filename)
+    {
+        boolean bOk = true;
+        try
+        {
+            if (reader != null)
+                reader.close();
+        }
+        catch (IOException ioException)
+        {
+            reader = null;
+        }
+        try
+        {
+            reader = new BufferedReader(new FileReader(filename));
+        }
+        catch (FileNotFoundException fileNotFoundException)
+        {
+            bOk = false;
+            reader = null;
+        }
+        return bOk;
+    }
+
+    public String getNextLine()
+    {
+        String lineRead = null;
+        try
+        {
+            if (reader != null)
+            {
+                lineRead = reader.readLine();
+                if (lineRead == null)
+                {
+                    reader.close();
+                }
+            }
+        }
+        catch (IOException ioException)
+        {
+            lineRead = null;
+        }
+        return lineRead;
+    }
+
+    public void closeFile()
+    {
+        try
+        {
+            reader.close();
+        }
+        catch (IOException ioException)
+        {
+            System.out.println("Fail to close file");
+        }
+    }
+}
