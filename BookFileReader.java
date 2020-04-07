ab83576684961c02fff5e09baf65bb6fb6936d88
Pisut-1037 <pisut.peter@mail.kmutt.ac.th>
2020-04-08 00:58:08 +0700

    keep making logic check command

--- BookFileReader.java
+++ BookFileReader.java
@@ -2,4 +2,40 @@
 public class BookFileReader extends TextFileReader
 {
     private String filename = "backup/books.txt";
+
+    private boolean checkBookCommand(String line)
+    {
+        String fields[] = line.split(";");
+        if (fields.length == 8){
+            // ยังไม่เช็ค attribute อื่นๆ
+            return true;
+        }
+        return false;
+    }
+
+    public Book readBook()
+    {
+        Book newBook = null;
+        String line;
+        Boolean bOk = (openFile(filename));
+        if (bOk)
+        {
+            do
+            {
+                line = getNextLine();
+                if (line != null)
+                {
+                    if (checkBookCommand(line))
+                    {
+
+                    }
+                    else
+                    {
+                        System.out.println("Invalid command");
+                    }
+                }
+            } while (newBook == null && line != null);
+        }
+        return newBook;
+    }
 }
