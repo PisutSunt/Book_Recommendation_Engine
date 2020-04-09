044f98e60de677b778121c8500c88462d74edec7
Pisut-1037 <pisut.peter@mail.kmutt.ac.th>
2020-04-09 19:02:08 +0700

    complete constructor

--- Book.java
+++ Book.java
@@ -1,4 +1,43 @@
 
-public class Book {
+public class Book
+{
+    private String title;
+    private String author;
+    private String genre;
+    private int lengthInPages;
+    private String keywords;
+    private float price;
+    private int totalBought = 0;
+    private String bookAbstract;
+    private int remaining;
+    private String ISBN;
 
+    public Book(String title, String author, String genre, int lengthInPages, float price, int totalBought, String bookAbstract, int remaining, String ISBN)
+    {
+        this.title = title;
+        this.author = author;
+        this.genre = genre;
+        this.lengthInPages = lengthInPages;
+        this.price = price;
+        this.totalBought = totalBought;
+        this.bookAbstract = bookAbstract;
+        this.remaining = remaining;
+        this.ISBN = ISBN;
+        keywords = title + " " + author + " " + genre + " " +
+                    bookAbstract + " " + ISBN;
+    }
+
+    public Book(String title, String author, String genre, int lengthInPages, float price, String bookAbstract, int remaining, String ISBN)
+    {
+        this.title = title;
+        this.author = author;
+        this.genre = genre;
+        this.lengthInPages = lengthInPages;
+        this.price = price;
+        this.bookAbstract = bookAbstract;
+        this.remaining = remaining;
+        this.ISBN = ISBN;
+        keywords = title + " " + author + " " + genre + " " +
+                    bookAbstract + " " + ISBN;
+    }
 }
