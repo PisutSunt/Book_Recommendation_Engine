fe964911a5153e1e4132525a3b5da38970570276
Pisut-1037 <pisut.peter@mail.kmutt.ac.th>
2020-04-10 05:14:51 +0700

    Complete read profile

---
+++ ProfileFileReader.java
@@ -0,0 +1,48 @@
+import java.util.ArrayList;
+
+public class ProfileFileReader extends TextFileReader
+{
+    private String filename = "backup/profiles.txt";
+
+    private boolean checkProfileCommand(String line)
+    {
+        String fields[] = line.split(";");
+        if (fields.length >= 4 && fields.length <= 8)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    public Profile readProfile()
+    {
+        Profile newProfile = null;
+        String line;
+        Boolean bOk = (openFile(filename));
+        if (bOk)
+        {
+            do
+            {
+                line = getNextLine();
+                if (line != null)
+                {
+                    if (checkProfileCommand(line))
+                    {
+                        String fields[] = line.split(";");
+                        ArrayList<String> genre = new ArrayList<String>();
+                        for (int i = 3; i < fields.length; i++)
+                        {
+                            genre.add(fields[i]);
+                        }
+                        newProfile = new Profile(fields[0], fields[1], fields[2], genre);
+                    }
+                    else
+                    {
+                        System.out.println("Invalid command");
+                    }
+                }
+            } while (newProfile == null && line != null);
+        }
+        return newProfile;
+    }
+}
