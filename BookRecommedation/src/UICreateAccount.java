19c01dc4ff133146aaa80d7c17a1af1cf7a9f8e1
aicquewk <vuttithatkrongyot@gmail.com>
2020-04-11 04:48:42 +0700

    UI class

--- BookRecommedation/src/UICreateAccount.java
+++ BookRecommedation/src/UICreateAccount.java
@@ -1,50 +1,22 @@
-import java.awt.EventQueue;
-
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.JFrame;
 
-public class UICreateAccount
+public class UICreateAccount extends JFrame 
+						implements ActionListener
 {
 
-	private JFrame frame;
-
-	/**
-	 * Launch the application.
-	 */
 	public static void main(String[] args)
 	{
-		EventQueue.invokeLater(new Runnable()
-		{
-			public void run()
-			{
-				try
-				{
-					UICreateAccount window = new UICreateAccount();
-					window.frame.setVisible(true);
-				}
-				catch (Exception e)
-				{
-					e.printStackTrace();
-				}
-			}
-		});
+	
+
 	}
 
-	/**
-	 * Create the application.
-	 */
-	public UICreateAccount()
+	
+	public void actionPerformed(ActionEvent e)
 	{
-		initialize();
-	}
-
-	/**
-	 * Initialize the contents of the frame.
-	 */
-	private void initialize()
-	{
-		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		
 	}
 
 }
