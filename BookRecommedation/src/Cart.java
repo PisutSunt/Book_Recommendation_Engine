f23a79f48c4dc4216215064702c7dbffbf0a6b12
altrr <peterpisut@gmail.com>
2020-04-15 23:26:29 +0700

    finish getTotalPrice & getSelectedBooks [file-sys]

--- BookRecommedation/src/Cart.java
+++ BookRecommedation/src/Cart.java
@@ -1,4 +1,25 @@
+import java.util.ArrayList;
+import javafx.util.Pair;
+
 public class Cart
 {
+    private ArrayList<Pair<Book, Integer>> selectedBooks;
 
+    public float getTotalPrice()
+    {
+        float totalPrice = 0;
+        for (Pair<Book,Integer> pair : selectedBooks)
+        {
+            for (int i = 0; i < pair.getValue(); i++)
+            {
+                totalPrice += pair.getKey().getPrice();
+            }
+        }
+        return totalPrice;
+    }
+
+    public ArrayList<Pair<Book, Integer>> getSelectedBooks()
+    {
+        return selectedBooks;
+    }
 }
