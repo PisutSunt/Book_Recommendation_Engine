31121f033edaa1c891ae6e8b9da93c19e5aae967
altrr <peterpisut@gmail.com>
2020-05-20 14:06:58 +0700

    add backup file

--- BookRecommedation/src/TestClass.java
+++ BookRecommedation/src/TestClass.java
@@ -2,6 +2,9 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+
+import javafx.util.Pair;
 
 public class TestClass
 {
@@ -10,21 +13,11 @@ public class TestClass
 		// WelcomePage welcome = new WelcomePage();
 		String[] e = {"s", "ee", "qe"};
 		Profile p = new Profile("userName", "email", "password", e);
+		Book b = new Book("title", "author", "genre", 1, 30, "bookAbstract", 9, "ISBN");
+		ArrayList<Pair<Book, Integer>> a = new ArrayList<Pair<Book, Integer>>();
+		Bill bill = new Bill(1, p, a, "receiver", "shippAddress", 2, 88888908978789L);
 		ProfileCollecionFileWriter.WriteObjectToFile(p);
-
-		try {
-            FileInputStream fileIn = new FileInputStream(".\\backup\\obj");
-            ObjectInputStream objectIn = new ObjectInputStream(fileIn);
- 
-            Object obj = objectIn.readObject();
- 
-            System.out.println("The Object has been read from the file");
-			objectIn.close();
-			System.out.println(obj.getClass());
-			System.out.println(((Profile)obj).getUserName());
- 
-        } catch (Exception ex) {
-            ex.printStackTrace();
-        }
+		BookCollectionFileWriter.WriteObjectToFile(b);
+		BillCollectionFileWriter.WriteObjectToFile(bill);
 	}
 }
