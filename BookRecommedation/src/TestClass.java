378dbb82e924dc6e2fe93617dddc8a18a6047c40
aicquewk <vuttithatkrongyot@gmail.com>
2020-05-21 16:39:57 +0700

    change directory of backup

--- BookRecommedation/src/TestClass.java
+++ BookRecommedation/src/TestClass.java
@@ -3,6 +3,8 @@ public class TestClass
 	public static void main(String[] args) 
 	{
 //		WelcomePage welcome = new WelcomePage();
+		ProfileCollection.initialize();
+		ProfileCollection.showAllProfile();
 		Index.showIndex();
 		
 	}
