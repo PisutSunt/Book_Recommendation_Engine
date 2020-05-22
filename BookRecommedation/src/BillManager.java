797f1478c074a5c221c68140361156014949fafa
altrr <peterpisut@gmail.com>
2020-05-22 21:15:01 +0700

    change ArrayList to Hashtable

--- BookRecommedation/src/BillManager.java
+++ BookRecommedation/src/BillManager.java
@@ -1,23 +1,25 @@
 import java.util.ArrayList;
+import java.util.Hashtable;
+
 import javafx.util.Pair;
 
 public class BillManager 
 {
-    private static ArrayList<BillCollection> allBillCollection;
+    private static Hashtable<String, BillCollection> allBillCollection = new Hashtable<String, BillCollection>();
     private static int billCounter = 0;
 
-    public static Bill searchBill(int billNo)
-    {
-        for (BillCollection billCollection : allBillCollection) 
-        {
-            for (Bill bill : billCollection.getBillCollection()) 
-            {
-                if (bill.getBillNo() == billNo)
-                    return bill;
-            }
-        }
-        return null;
-    }
+    // public static Bill searchBill(int billNo)
+    // {
+    //     for (BillCollection billCollection : allBillCollection) 
+    //     {
+    //         for (Bill bill : billCollection.getBillCollection()) 
+    //         {
+    //             if (bill.getBillNo() == billNo)
+    //                 return bill;
+    //         }
+    //     }
+    //     return null;
+    // }
 
     public static void createBill(Profile buyer, ArrayList<Pair<Book, Integer>> boughtBooks, 
                             String receiver, String shippingAddress, float totalPrice)
