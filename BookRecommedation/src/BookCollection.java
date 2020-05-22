090401eebd9dfd8415611641a78802436874095a
aicquewk <vuttithatkrongyot@gmail.com>
2020-05-22 20:41:50 +0700

    change to Hashtable

--- BookRecommedation/src/BookCollection.java
+++ BookRecommedation/src/BookCollection.java
@@ -1,24 +1,69 @@
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.Dictionary;
 import java.util.Hashtable;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class BookCollection
 {
-    public static Dictionary<String, Book> bookCollection = new Hashtable<String, Book>();
-    private ArrayList<Book> bookToShow;
+	public static Hashtable<String, Book> bookCollection = new Hashtable<String, Book>();
 
-    public static Book getBook(String keyword)
-    {
-        return bookCollection.get(keyword);
-    }
+	public static Book getBook(String keyword)
+	{
+		return bookCollection.get(keyword);
+	}
 
-    public static void addBook(Book book)
-    {
-        bookCollection.put(book.getKeywords(), book);
-    }
+	public static void addBook(Book book)
+	{
+		bookCollection.put(book.getKeywords(), book);
+	}
 
-    public static Dictionary<String, Book> getBookCollection()
-    {
-        return bookCollection;
-    }
+	public static Hashtable<String, Book> getBookCollection()
+	{
+		return bookCollection;
+	}
+
+	public static boolean searchBooks(String keywords, String field)
+	{
+		Set<String> set = bookCollection.keySet().stream().filter(s -> s.contains(keywords))
+				.collect(Collectors.toSet());
+
+		if(set.isEmpty())
+		{
+			System.out.println("*** No item match your search ***");
+			return false;
+		}
+		else
+		{
+			String methodName = field;
+			Class[] parameterType = null;
+			Book book = new Book("", "", "", 0, 0, "", 0, "");
+
+			try
+			{
+				Method method = book.getClass().getMethod(methodName, parameterType);
+				try
+				{
+					for (String key : set)
+					{
+						book = bookCollection.get(key);
+						System.out.println(method.invoke(book));
+					}
+				}
+				catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException e)
+				{
+					e.printStackTrace();
+				}
+			}
+			catch (NoSuchMethodException | SecurityException e)
+			{
+				e.printStackTrace();
+			}
+			
+			return true;
+		}
+		
+	}
 }
