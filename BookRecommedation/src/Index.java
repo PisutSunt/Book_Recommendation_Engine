68a36fa04ef8410c67caf021f236b2387db83da3
altrr <peterpisut@gmail.com>
2020-05-23 14:04:04 +0700

    comment...

--- BookRecommedation/src/Index.java
+++ BookRecommedation/src/Index.java
@@ -1,20 +1,24 @@
 /**
- * The class Index represents
+ * The class Index represents the index page that print
+ * alternative action between creating account and logging in
+ * 
+ * Created by Pisut Suntronkiti  ID: 60070501037
+ *            Wuttithat Krongyot ID: 60070501084
  */
 public class Index
 {
-	
+	/**
+	 * Print the user interface of index page
+	 */
 	public static void showIndex()
 	{
 		int selMenu;
-		
 		System.out.println("------------------ Welcome to book recommended engine! ------------------");
 		System.out.println("\tPlease select menu number!");
 		System.out.println("\t1. Login");
 		System.out.println("\t2. Create account");
 		
 		selMenu = IOUtils.checkInputMenu(1, 2);
-		
 		switch(selMenu)
 		{
 			case 1:
@@ -26,7 +30,5 @@ public class Index
 			default:
 				break;
 		}
-		
-		
 	}
 }
